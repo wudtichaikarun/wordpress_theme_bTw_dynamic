337210ab0b72584d9bcff57bf7541acf3cbe591d
Romantic <595159090004@dpu.ac.th>
2018-03-26 12:52:28 +0700

    56. dynamic section instructor

--- page-home.php
+++ page-home.php
@@ -32,6 +32,16 @@ $features_section_body = get_field('features_section_body');
 $project_feature_title = get_field('project_feature_title');
 $project_feature_body = get_field('project_feature_body');
 
+$instructor_section_title = get_field('instructor_section_title');
+$instructor_name = get_field('instructor_name');
+$bio_excerpt = get_field('bio_excerpt');
+$full_bio = get_field('full_bio');
+$twitter_username = get_field('twitter_username');
+$facebook_username = get_field('facebook_username');
+$google_plus_username = get_field('google_plus_username');
+$num_students = get_field('number_students');
+$num_reviews = get_field('number_reviews');
+$num_courses = get_field('number_courses');
 
 get_header(); ?>
 
@@ -241,28 +251,36 @@ get_header(); ?>
         <div class="col-sm-8 col-md-6">
           <div class="row">
             <div class="col-lg-8">
-              <h2>You Instructor <small>Brad Hussey</small></h2>
+              <h2><?php echo $instructor_section_title; ?> <small><?php echo $instructor_name; ?></small></h2>
             </div>
 
             <div class="col-lg-4">
-              <a href="" target="_blank" class="badge social twitter">
-                <i class="fa fa-twitter"></i>
-              </a>
 
-              <a href="" target="_blank" class="badge social facebook">
-                <i class="fa fa-facebook"></i>
-              </a>
+              <?php if( !empty($twitter_username) ) : ?>
+                <a href="https://twitter.com/<?php echo $twitter_username; ?>" target="_blank" class="badge social twitter">
+                  <i class="fa fa-twitter"></i>
+                </a>
+              <?php endif; ?>
 
-              <a href="" target="_blank" class="badge social gplus">
-                <i class="fa fa-google-plus"></i>
-              </a>
+
+              <?php if( !empty($facebook_username) ) : ?>
+                <a href="https://facebook.com/<?php echo $facebook_username; ?>" target="_blank" class="badge social facebook">
+                  <i class="fa fa-facebook"></i>
+                </a>
+              <?php endif; ?>
+
+              <?php if( !empty($google_plus_username) ) : ?>
+                <a href="https://plus.google.com/<?php echo $google_plus_username; ?>" target="_blank" class="badge social gplus">
+                  <i class="fa fa-google-plus"></i>
+                </a>
+              <?php endif; ?>
+              
             </div>
           </div>
 
-          <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dolore, iure laborum delectus quos inventore impedit sit provident accusantium magnam.</p>
-          <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dolore, iure laborum delectus quos inventore impedit sit provident accusantium magnam.</p>
-          <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dolore, iure laborum delectus quos inventore impedit sit provident accusantium magnam.</p>
-          <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dolore, iure laborum delectus quos inventore impedit sit provident accusantium magnam.</p>
+          <p class="lead"><?php echo $bio_excerpt; ?></p>
+
+          <?php echo $full_bio; ?>
 
           <hr>
 
@@ -273,7 +291,7 @@ get_header(); ?>
             <div class="col-xs-4">
               <div class="num">
                 <div class="num-content">
-                  41,000+ <span>student</span>
+                  <?php echo $num_students; ?> <span>student</span>
                 </div>
               </div>
             </div>
@@ -281,7 +299,7 @@ get_header(); ?>
             <div class="col-xs-4">
               <div class="num">
                 <div class="num-content">
-                  568 <span>review</span>
+                  <?php echo $num_reviews; ?><span>review</span>
                 </div>
               </div>
             </div>
@@ -289,7 +307,7 @@ get_header(); ?>
             <div class="col-xs-4">
               <div class="num">
                 <div class="num-content">
-                  8 <span>courses</span>
+                   <?php echo $num_courses; ?> <span>courses</span>
                 </div>
               </div>
             </div>
